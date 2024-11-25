93993fc67057a328936dd24bc9446d6763b0bb5f
22bolts <bguy00504@gmail.com>
2024-11-25 23:18:15 +0100

    upload thing fix

--- src/utils/uploadthing.ts
+++ src/utils/uploadthing.ts
@@ -1,10 +1,7 @@
-import { generateComponents } from '@uploadthing/react';
-// export type { UploadFileResponse } from 'uploadthing/client';
-import { generateReactHelpers } from '@uploadthing/react/hooks';
-import type { OurFileRouter } from '@/app/api/uploadthing/core';
-
-export const { UploadButton, UploadDropzone, Uploader } =
-  generateComponents<OurFileRouter>();
+import { createUploadthing } from "uploadthing/next";
+import { UploadButton, UploadDropzone } from "@uploadthing/react";
+import { generateReactHelpers } from "@uploadthing/react";
+import type { OurFileRouter } from "@/app/api/uploadthing/core";
 
 export const { useUploadThing, uploadFiles } =
   generateReactHelpers<OurFileRouter>();
