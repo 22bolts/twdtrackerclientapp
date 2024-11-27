8714a0717cc8e96a9807a3f4b1831acb106978fb
22bolts <bguy00504@gmail.com>
2024-11-27 14:22:25 +0100

    page show try 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,52 @@
+// /** @type {import('next').NextConfig} */
+// import path from 'path';
+
+// const nextConfig = {
+//   webpack: (config, { isServer }) => {
+//     config.resolve.alias['@'] = path.resolve('./src');
+//     config.resolve.alias['@public'] = path.resolve('./public');
+//     return config;
+//   },
+//   images: {
+//     domains: [
+//       'localhost',
+//       'randomuser.me',
+//       'cloudflare-ipfs.com',
+//       'avatars.githubusercontent.com',
+//       'picsum.photos',
+//       'flagcdn.com',
+//       'utfs.io',
+//       'images.unsplash.com',
+//       's3.amazonaws.com',
+//       'isomorphic-furyroad.s3.amazonaws.com',
+//       'img.freepik.com',
+//     ],
+//   },
+//   env: {
+//     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+//     NEXTAUTH_URL: process.env.NEXTAUTH_URL
+//   },
+//   reactStrictMode: false,
+// };
+
+// export default nextConfig;
+
+
+
+
+
+
+
+
+
+
+// // ${{ secrets.NEXTAUTH_SECRET }}
+
+
+
+
 /** @type {import('next').NextConfig} */
 import path from 'path';
-
 const nextConfig = {
   webpack: (config, { isServer }) => {
     config.resolve.alias['@'] = path.resolve('./src');
@@ -26,18 +72,8 @@ const nextConfig = {
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL
   },
+  output: 'export', // Add this line
+  trailingSlash: true, // Add this line
   reactStrictMode: false,
 };
-
 export default nextConfig;
-
-
-
-
-
-
-
-
-
-
-// ${{ secrets.NEXTAUTH_SECRET }}
